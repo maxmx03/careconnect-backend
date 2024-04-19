1830480578a8bf4cdc148ad56d2235e7b41c0d5b
Max Miliano <milianordelcanto@gmail.com>
2024-04-19 10:29:20 -0300

    fix: create_user_table migration

--- mysql/migrations/000001_create_user_table.up.sql
+++ mysql/migrations/000001_create_user_table.up.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS user (
     user_id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255),
     email VARCHAR(255),
-    type ENUM('doctor', 'patient')
-    password VARCHAR(255),
+    type ENUM('doctor', 'patient'),
+    password VARCHAR(255)
 );
