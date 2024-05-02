d42225a794c8a04d6d74920028ba89d514502f4a
Max Miliano <milianordelcanto@gmail.com>
2024-05-02 15:55:54 -0300

    refactor: doctor,patient tables

--- mysql/migrations/000003_create_patient_table.up.sql
+++ mysql/migrations/000003_create_patient_table.up.sql
@@ -1,8 +1,10 @@
 CREATE TABLE patient (
     patient_id INT PRIMARY KEY AUTO_INCREMENT,
+    doctor_id INT,
     cpf VARCHAR(255),
     date_of_birth DATE,
     username VARCHAR(255),
-    password VARCHAR(255)
+    password VARCHAR(255),
+    FOREIGN KEY (doctor_id) REFERENCES doctor(doctor_id),
 );
 
