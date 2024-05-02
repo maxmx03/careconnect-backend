b2667ca0fc3d31bd300b3d398a46dac75b911e05
Max Miliano <milianordelcanto@gmail.com>
2024-05-02 15:53:48 -0300

    feat: drop medication table

--- mysql/migrations/000005_create_medication_table.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS medication (
-    medication_id INT PRIMARY KEY AUTO_INCREMENT,
-    patient_id INT,
-    name VARCHAR(255),
-    dosage VARCHAR(255),
-    schedule VARCHAR(255),
-    FOREIGN KEY (patient_id) REFERENCES patient(patient_id)
-);
-
