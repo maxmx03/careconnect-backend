f7dab36bbe472fbda8f93eeb5d5f0639e41e9900
Max Miliano <milianordelcanto@gmail.com>
2024-05-09 16:20:38 -0300

    feat: packages removed

--- doctorconsultation/model.go
+++
@@ -1,9 +0,0 @@
-package doctorconsultation
-
-type MedicalConsultationModel struct {
-	ConsultationID int    `json:"consultation_id"`
-	DoctorID       string `json:"doctor_crm"`
-	PatientID      int    `json:"patient_id"`
-	DateTime       string `json:"date_time"`
-	Description    string `json:"description"`
-}
