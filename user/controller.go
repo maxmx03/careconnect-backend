73e4baaf1902a37a084b81e1b5915e03d3cb2da3
Max Miliano <milianordelcanto@gmail.com>
2024-05-23 15:23:54 -0300

    feat: user router and controllers

--- user/controller.go
+++ user/controller.go
@@ -3,8 +3,10 @@ package user
 import (
 	"database/sql"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
 	. "github.com/maxmx03/careconnect-backend/feedback"
 )
 
@@ -26,4 +28,59 @@ func (a *UserController) Login(c echo.Context, db *sql.DB) error {
 	}
 
 	return c.JSON(http.StatusOK, map[string]string{"token": token})
+}
+
+func (a *UserController) Create(c echo.Context, db *sql.DB) error {
+	user := &UserModel{}
+
+	if err := c.Bind(user); err != nil {
+		return err
+	}
+
+  err := userService.Create(user, db)
+
+  if err!= nil {
+    log.Error(err)
+    return c.JSON(http.StatusInternalServerError, GetError("Failed to create user"))
+  }
+
+  return c.JSON(http.StatusCreated, GetOk("User created successfully"))
+}
+
+func (u *UserController) Update(c echo.Context, db *sql.DB) error {
+	user := &UserModel{}
+
+	if err := c.Bind(user); err != nil {
+		return err
+	}
+
+	id, err := strconv.Atoi(c.Param("id"))
+
+	if err != nil {
+		return c.JSON(http.StatusNotFound, GetError("Invalid doctor id"))
+	}
+
+	err = userService.Update(user, id, db)
+
+	if err != nil {
+		log.Error(err)
+		return c.JSON(http.StatusNotFound, GetError("Failed to update user"))
+	}
+
+	return c.JSON(http.StatusOK, GetOk("User updated successfully"))
+}
+
+func (u *UserController) Delete(c echo.Context, db *sql.DB) error {
+	id, err := strconv.Atoi(c.Param("id"))
+
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, GetError("Invalid user id"))
+	}
+
+	if err = userService.Delete(id, db); err != nil {
+		log.Error(err)
+		return c.JSON(http.StatusNotFound, GetError("Failed to delete user"))
+	}
+
+	return c.JSON(http.StatusOK, GetOk("User deleted successfully"))
 }
--- user/routes.go
+++ user/routes.go
@@ -8,7 +8,18 @@ import (
 var userController = &UserController{}
 
 func Routes(e *echo.Echo, db *sql.DB) {
-	e.POST("/login", func(c echo.Context) error {
+	var url = "/users"
+	g := e.Group(url)
+	g.POST("/login", func(c echo.Context) error {
 		return userController.Login(c, db)
+	})
+	g.POST("", func(c echo.Context) error {
+		return userController.Create(c, db)
+	})
+	g.PUT("/:id", func(c echo.Context) error {
+		return userController.Update(c, db)
+	})
+	g.DELETE("/:id", func(c echo.Context) error {
+		return userController.Delete(c, db)
 	})
 }
